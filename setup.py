1b4a370bf039bc9c9050dcc3c34048d6eea733e2
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-26 11:43:36 -0400

    Basic infrastructure for a package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+execfile('cmph/cmph_version.py')
+long_description = file('README.rst').read()
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development',
+]
+
+setup(name = 'cmph',
+      version = __version__,
+      description = 'Wrapper for CMPH',
+      long_description = long_description,
+      author = 'Bueda',
+      author_email = 'support@bueda.com',
+      license = 'MIT',
+      platforms = ['Any'],
+      classifiers = classifiers,
+      url = 'http://www.bueda.com',
+      packages = ['cmph'],
+      )
+
+
